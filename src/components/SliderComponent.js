26adaf38207997dd6464405c90eb7a08cc81f4ef
ishbayev16 <anvar.ishbaev@sarente.com>
2021-07-07 11:12:14 +0300

    layout

---
+++ src/components/SliderComponent.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import Slider from "@farbenmeer/react-spring-slider";
+
+
+function Slider() {
+
+
+    return (
+        <Slider>
+            <div>child 1</div>
+            <div>child 2</div>
+            <div>child 3</div>
+        </Slider>
+    );
+}
+
+export default Slider;
